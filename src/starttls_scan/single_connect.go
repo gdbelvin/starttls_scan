38b9cf49914611e5c09411eb4b61226c8c620e2c
Urbanus <urbanus@Urbanuss-MacBook-Air.local>
2014-03-18 18:00:51 -0700

    Custom smtp lib

---
+++ src/starttls_scan/single_connect.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "crypto/tls"
+    "fmt"
+    "net/smtp"
+    "reflect"
+    "unsafe"
+    "os"
+    "net"
+)
+
+type ConnInfo struct {
+    domain string
+    hastls bool
+    tlssuccess bool
+    tcp bool
+    conn tls.Conn
+}
+
+func readConn(client *smtp.Client) {
+    //v := reflect.ValueOf(*client)
+    //conn := v.FieldByName("conn")
+    //tconn, err := conn.Interface(false).(tls.Conn)
+
+    pointerVal := reflect.ValueOf(client)
+    val := reflect.Indirect(pointerVal)
+
+    member := val.FieldByName("conn")
+    ptrToY := unsafe.Pointer(member.UnsafeAddr())
+    realPtrToY := (**net.Conn)(ptrToY)
+    fmt.Println("ptr:   ", realPtrToY)
+    fmt.Println("*ptr:  ", *realPtrToY)
+    fmt.Println("**ptr: ", **realPtrToY)
+    //*realPtrToY = nil // or &Foo{} or whatever
+}
+
+func DoConnect(addr string) (ConnInfo, error) {
+    ret := ConnInfo{addr, false, false, false, tls.Conn{}}
+    serverName := "localhost"
+
+    c, err := smtp.Dial(addr)
+    if err != nil {
+        return ret, err
+    }
+    ret.tcp = true
+    defer c.Close()
+    if err = c.Hello(serverName); err != nil {
+        return ret, err
+    }
+    if ok, _ := c.Extension("STARTTLS"); ok {
+        ret.hastls = true
+        //config := &tls.Config{ServerName: serverName}
+        if err = c.StartTLS(nil); err != nil {
+            return ret, err
+        }
+        ret.tlssuccess = true
+        // At this point, convert c.conn 
+        readConn(c)
+    }
+    if err != nil {
+        return ret, err
+    }
+    return ret, c.Quit()
+}
+
+
+func main() {
+    dest := os.Args[1]
+    fmt.Println("Connecting to: ", dest)
+    info, err := DoConnect(dest)
+    if err != nil {
+       fmt.Println("error: ", err)
+    }
+    fmt.Println(info)
+    fmt.Println("conn: ", info.conn)
+}
